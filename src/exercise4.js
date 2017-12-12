901a75aab5ca6b1d93cea11b17ba4842c35bf45b
Maxime COURANT <maxime.courant@ynov.com>
2017-12-12 16:15:28 +0100

    Exercice 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,7 +1,15 @@
 exports.run = function(input) {
   console.log("Exercise #4");
 
-  // YOUR CODE IS WELCOME HERE
+  function myFunction(b,c){
+    var result = "";
+    var tmp = "";
+    c.forEach( (element, index) => {
+        index === b.length-1 ? tmp = "" : tmp = ", "
+        result += "The value of "+element+ " is "+ b[index]+tmp;
+    });
+    return result;
+  }
 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
